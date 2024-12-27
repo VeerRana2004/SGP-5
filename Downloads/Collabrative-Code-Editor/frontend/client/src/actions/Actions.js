30aa6f5cb51cab3f7db6cd45fa75bf805bb17681
VeerRana2004 <121330113+VeerRana2004@users.noreply.github.com>
2024-12-27 15:01:38 +0530

    Delete Downloads directory

--- Downloads/Collabrative-Code-Editor/frontend/client/src/actions/Actions.js
+++
@@ -1,8 +0,0 @@
-const ACTIONS = {
-    JOIN: 'join',
-    JOINED: 'joined',
-    CODE_CHANGE: 'code-change',
-    SYNC_CODE: 'sync-code',
-    DISCONNECTED: 'disconnected',
-};
-module.exports =ACTIONS
--- Downloads/Collabrative-Code-Editor/frontend/client/src/socket.js
+++
@@ -1,7 +0,0 @@
-
-import io from 'socket.io-client';
-
-export const initSocket = () => {
-    const socket = io('http://localhost:5001'); // Replace with your server URL
-    return socket;
-};
